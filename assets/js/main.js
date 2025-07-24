c6159eaee645492bbebb41677ed7431141fbdaa6
Aládio Vanderlei de Lima Júnior <109392329+A1AD10@users.noreply.github.com>
2025-07-24 16:58:44 -0300

    correção portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,14 @@ function updateLanguages(profileData) {
 
 function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio')
-    portfolio.innerHTML = profileData.portfolio.map(portfolio => `<li>${portfolio}</li>`).join('')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
 }
 
 (async () => {
